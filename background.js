951ec381d756fbbe0f56d98a2b59f4f2e973649d
sangyong park <76932869+sangyoung23@users.noreply.github.com>
2021-12-28 21:38:00 +0900

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-const images = ["01.jpg" , "02.jpg", "03.jpg"];
-
-const randomImages = images[Math.floor(Math.random()*images.length)];
-
-const bgImages = document.createElement("img");
-
-bgImages.src = `IMG/${randomImages}`;
-
-document.body.appendChild(bgImages);
