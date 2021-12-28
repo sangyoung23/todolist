c7abbdec7b49c59da4d3d85642c3ab34ea31125a
sangyong park <76932869+sangyoung23@users.noreply.github.com>
2021-12-28 21:39:03 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const loginForm = document.querySelector("#loginForm");
+const loginInput = document.querySelector("#loginForm input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+// const USERNAME_KEY = username;
+
+function onLogInSubmit (event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem("username", username);
+    paintGreeting(username);
+}
+
+function paintGreeting (username) {
+    greeting.innerText = `Hello! ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUserName = localStorage.getItem("username");
+
+if (savedUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLogInSubmit);
+} else {
+    paintGreeting(savedUserName);
+}
